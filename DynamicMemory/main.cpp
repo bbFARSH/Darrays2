11c5e1ad293358e7e18aa56845452b86629c3b6e
Sali_dk52@itstepru.onmicrosoft.com <Sali_dk52@itstepru.onmicrosoft.com>
2021-09-14 19:13:57 +0300

    DynamicMemory

---
+++ DynamicMemory/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using std::cin;
+using std::endl;
+using std::cout;
+
+void FillRand(int arr[], const unsigned int n);
+void Print(int arr[], const unsigned int n);
+
+int* push_back(int arr[], int& n, int value);
+int* pop_back(int arr[], int& n);
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "¬ведите размер массива: "; cin >> n;
+	int* arr = new int[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	int value;
+	cout << " ¬ведите добавл€емое значение : "; cin >> value;
+	arr = push_back(arr, n, value);
+	Print(arr, n);
+	arr = pop_back(arr, n);
+	//n++;
+	Print(arr, n);
+	delete[] arr;
+}
+void FillRand(int arr[], const unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+
+}
+void Print(int arr[], const unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+
+}
+int* push_back(int arr[], int& n, int value)
+{
+	Print(arr, n);
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	//3) ”дал€ем исходный массив
+	delete[] arr;
+	arr = buffer;
+	arr[n] = value;
+	n++;
+	return arr;
+}
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++) buffer[i] = arr[i];
+	delete[]	arr;
+	return buffer;
+
+}
